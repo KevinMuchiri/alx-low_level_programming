55f716e53dfc8e37c3e5b7dad1c11c08f8f1611f
KevinMuchiri <muchirimwangiafrika@gmail.com>
2022-06-05 05:22:08 -0700

    update betty compliant

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
+ * main - use to return the integer value 1
  *
- * Description- prints string of characters using write fn
+ * Description: prints string of characters using write fn
  *
- * Returns a value of 1
+ * Return: return the value 1
+ *
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
